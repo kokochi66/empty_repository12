d9b82bbbc5737218ba07735bdd89515ce5cedf9b
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-07-28 19:21:28 +0900

    StudyFlim으로 리파지토리 이동, 정리

--- mst/src/main/java/com/memorial/st/mst/domain/music/model/MusicType.java
+++
@@ -1,9 +0,0 @@
-package com.memorial.st.mst.domain.music.model;
-
-public enum MusicType {
-    ANI,
-    GAME,
-    EROGE,
-    MISC
-    ;
-}
--- mst/src/main/java/com/memorial/st/mst/service/music/repository/MusicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.memorial.st.mst.service.music.repository;
-
-import com.memorial.st.mst.domain.music.Music;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicRepository extends JpaRepository<Music, Long> {
-}
